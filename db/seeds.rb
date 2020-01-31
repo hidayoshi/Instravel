5847bb076a80e133e2d868c566561d62878d21bc
hidayoshi <toto0818summer@gaail.com>
2020-01-31 21:44:48 +0900

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,14 @@ User.create!(name:  name,
 end
 
 users = User.order(:created_at).take(6)
-20.times do
+15.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.imageposts.create!(content: content) }
 end
+
+users = User.all
+user  = users.first
+following = users[2..40]
+followers = users[3..30]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
