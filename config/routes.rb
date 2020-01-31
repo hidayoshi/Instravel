c0a4d2d8c84af4ab2ae8b7cedacf1176f2c29e7e
hidayoshi <toto0818summer@gaail.com>
2020-01-31 10:32:42 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
 
   get '/signup', to: 'users#new'
@@ -9,6 +13,7 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
