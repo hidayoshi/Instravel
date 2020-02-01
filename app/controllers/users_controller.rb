7429cdc97724702e7a665a7c6c42a45bdd052f61
hidayoshi <toto0818summer@gaail.com>
2020-02-01 16:59:49 +0900

    管理者権限で投稿を削除できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,7 +80,4 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
 end
