274cdf632334cf3464d43c0b5edd5c50257957bc
hidayoshi <toto0818summer@gmail.com>
2020-02-05 15:41:44 +0900

    seedを本番環境で

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ User.create!(name:  "Example User",
     activated_at: Time.zone.now)
 
 50.times do |n|
-name  = Faker::Name.name
+name  = "name-#{n+1}"
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
 User.create!(name:  name,
